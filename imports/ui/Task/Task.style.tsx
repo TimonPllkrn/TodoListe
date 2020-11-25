13a1598c60a2a1fa7478b10271e0f5bdd8097bd3
Max Stephan <max.stephan@mni.thm.de>
2020-11-25 13:03:25 +0100

    TaskInfo: changed styling

--- imports/ui/Task/Task.style.tsx
+++ imports/ui/Task/Task.style.tsx
@@ -2,9 +2,18 @@ import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
+    root: {},
+    secondary: {
       display: "flex",
       flexFlow: "row",
+      margin: "5px",
+    },
+    small: {
+      width: theme.spacing(3),
+      height: theme.spacing(3),
+    },
+    chip: {
+      marginRight: "5px",
     },
   })
 );
