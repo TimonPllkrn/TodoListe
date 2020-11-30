c48efc7c27a1d7b297adb36b40ee4716ca1feebf
Jannik-PC <jannik.lapp@mni.thm.de>
2020-11-30 10:24:29 +0100

    reseted to 2 spaces

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -11,77 +11,77 @@ import {User} from "../types/User";
 
 // hard coded categories
 export const Categories: Category[] = [
-    {
-        _id: "1",
-        name: "Uni",
-        color: "#FF968F"
-    },
-    {
-        _id: "2",
-        name: "Arbeit",
-        color: "#99C0FF"
-    },
-    {
-        _id: "3",
-        name: "Haushalt",
-        color: "#7BDE76"
-    }
+  {
+    _id: "1",
+    name: "Uni",
+    color: "#FF968F"
+  },
+  {
+    _id: "2",
+    name: "Arbeit",
+    color: "#99C0FF"
+  },
+  {
+    _id: "3",
+    name: "Haushalt",
+    color: "#7BDE76"
+  }
 ]
 
 export const getCategory = (_id: string): Category | undefined => {
-    return Categories.find(c => c._id === _id);
+  return Categories.find(c => c._id === _id);
 }
 
 // TODO: create UsersCollection
 export const Users: User[] = [
-    {
-        _id: "1",
-        name: "David"
-    },
-    {
-        _id: "2",
-        name: "Jannik"
-    },
-    {
-        _id: "3",
-        name: "Max"
-    },
-    {
-        _id: "4",
-        name: "Timon"
-    }
+  {
+    _id: "1",
+    name: "David"
+  },
+  {
+    _id: "2",
+    name: "Jannik"
+  },
+  {
+    _id: "3",
+    name: "Max"
+  },
+  {
+    _id: "4",
+    name: "Timon"
+  }
 ]
 
 export const getUser = (_id: string) => {
-    return Users.find(u => u._id === _id);
+  return Users.find(u => u._id === _id);
 }
 
 
 export const App = () => {
-    const classes = useStyles();
-    const doneTasks = useTracker(() => TasksCollection
-        .find({done: {$ne: false}}, {sort: {doneDate: 1}})
-        .fetch());
-    const tasks = useTracker(() => TasksCollection
-        .find({done: {$ne: true}}, {sort: {createDate: 1}})
-        .fetch());
+  const classes = useStyles();
+  const doneTasks = useTracker(() => TasksCollection
+    .find({done: {$ne: false}}, {sort: {doneDate: 1}})
+    .fetch());
+  const tasks = useTracker(() => TasksCollection
+    .find({done: {$ne: true}}, {sort: {createDate: 1}})
+    .fetch());
 
-    return (
-        <div>
-            <Header/>
-            <div className={classes.section}>
-                <Typography variant="h5">Create new Task</Typography>
-                <TaskDialog categories={[]}></TaskDialog>
-            </div>
-            <div className={classes.section}>
-                <Typography variant="h5">ToDo ({tasks.length})</Typography>
-                <TaskList tasks={tasks}/>
-            </div>
-            <Divider/>
-            <div className={classes.section}>
-                <Typography variant="h5">Done ({doneTasks.length})</Typography>
-                <TaskList tasks={doneTasks}/>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Header/>
+      <div className={classes.section}>
+        <Typography variant="h5">Create new Task</Typography>
+        <TaskDialog categories={[]}></TaskDialog>
+      </div>
+      <div className={classes.section}>
+        <Typography variant="h5">ToDo ({tasks.length})</Typography>
+        <TaskList tasks={tasks}/>
+      </div>
+      <Divider/>
+      <div className={classes.section}>
+        <Typography variant="h5">Done ({doneTasks.length})</Typography>
+        <TaskList tasks={doneTasks}/>
+      </div>
+    </div>
+  );
 };
--- imports/ui/TaskDialog/index.tsx
+++ imports/ui/TaskDialog/index.tsx
@@ -5,88 +5,88 @@ import {Category} from '/imports/types/Category';
 import {Priority} from '/imports/types/Priority';
 
 export interface NewButtonProps {
-    categories: Category[];
+  categories: Category[];
 }
 
 export const TaskDialog: React.FC<NewButtonProps> = ({categories}) => {
-    const [title, setTitle] = React.useState("");
-    const [priority, setPriority] = React.useState<Priority>(Priority.Medium);
-    const [category, setCategory] = React.useState<Category | undefined>(undefined);
+  const [title, setTitle] = React.useState("");
+  const [priority, setPriority] = React.useState<Priority>(Priority.Medium);
+  const [category, setCategory] = React.useState<Category | undefined>(undefined);
 
-    const handlePriorityChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
-        setPriority(event.target.value as Priority);
-    };
+  const handlePriorityChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+    setPriority(event.target.value as Priority);
+  };
 
-    const handleCategoryChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
-        setCategory(categories[event.target.value as number]);
-    };
+  const handleCategoryChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+    setCategory(categories[event.target.value as number]);
+  };
 
-    const handleSave = () => {
-        if (title.trim() === "") {
-            return;
-        }
-
-        TasksCollection.insert({
-            title: title,
-            category: category,
-            done: false,
-            ownerId: "-1",
-            priority: Number(priority),
-            createDate: new Date(),
-            doneDate: undefined
-
-        });
-        setTitle("");
-        setPriority(Priority.Medium);
-        setCategory(undefined)
-
+  const handleSave = () => {
+    if (title.trim() === "") {
+      return;
     }
 
-    return (
-        <div>
-            <Box mx={2}>
-                <TextField
-                    label="Title"
-                    margin="dense"
-                    value={title}
-                    onChange={(t) => {
-                        setTitle(t.target.value)
-                    }}
-                />
-            </Box>
+    TasksCollection.insert({
+      title: title,
+      category: category,
+      done: false,
+      ownerId: "-1",
+      priority: Number(priority),
+      createDate: new Date(),
+      doneDate: undefined
 
-            <Box mt={3} mx={2}>
-                <InputLabel htmlFor="priority">Priority</InputLabel>
-                <Select
-                    value={priority}
-                    onChange={handlePriorityChange}
-                    native
-                    inputProps={{
-                        id: 'priority'
-                    }}
-                >
-                    <option value={Priority.Low}>Low</option>
-                    <option value={Priority.Medium}>Medium</option>
-                    <option value={Priority.High}>High</option>
-                </Select>
-            </Box>
-            <Box my={3} mx={2}>
-                <InputLabel htmlFor="category">Category</InputLabel>
-                <Select
-                    value={""}
-                    onChange={handleCategoryChange}
-                    native
-                    inputProps={{
-                        id: 'category'
-                    }}
-                >
-                    <option value={undefined}>None</option>
-                    {categories.map(c => (<option value={c._id} key={c._id}>{c.name}</option>))}
-                </Select>
-            </Box>
-            <Button onClick={handleSave} color="primary">
-                Save
-            </Button>
-        </div>
-    )
+    });
+    setTitle("");
+    setPriority(Priority.Medium);
+    setCategory(undefined)
+
+  }
+
+  return (
+    <div>
+      <Box mx={2}>
+        <TextField
+          label="Title"
+          margin="dense"
+          value={title}
+          onChange={(t) => {
+            setTitle(t.target.value)
+          }}
+        />
+      </Box>
+
+      <Box mt={3} mx={2}>
+        <InputLabel htmlFor="priority">Priority</InputLabel>
+        <Select
+          value={priority}
+          onChange={handlePriorityChange}
+          native
+          inputProps={{
+            id: 'priority'
+          }}
+        >
+          <option value={Priority.Low}>Low</option>
+          <option value={Priority.Medium}>Medium</option>
+          <option value={Priority.High}>High</option>
+        </Select>
+      </Box>
+      <Box my={3} mx={2}>
+        <InputLabel htmlFor="category">Category</InputLabel>
+        <Select
+          value={""}
+          onChange={handleCategoryChange}
+          native
+          inputProps={{
+            id: 'category'
+          }}
+        >
+          <option value={undefined}>None</option>
+          {categories.map(c => (<option value={c._id} key={c._id}>{c.name}</option>))}
+        </Select>
+      </Box>
+      <Button onClick={handleSave} color="primary">
+        Save
+      </Button>
+    </div>
+  )
 }
